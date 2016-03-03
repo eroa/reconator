7d3ad61871fb2468d19c4c42bfa812547862fa41
toxic <toxic@t28.io>
2016-03-03 17:31:03 +0100

    t1

--- testnmap/t1.py
+++ testnmap/t1.py
@@ -6,12 +6,12 @@ import sys
 import nmap
 import libnmap
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     nm = nmap.PortScanner()
     print " RECONATOR : usage " + sys.argv[0] + "ip_list.txt"
     list = open(sys.argv[1])
     for ip in list:
         nm.command_line("nmap -sP %s " % (str(ip))
-        print ('host : %s(%s)' % (ip,nm._scan_result())))
+        print "host :" + (ip,nm._scan_result())
 
 
