124e032066c1422300e960db7e842441540654c0
toxic <toxic@murene>
2016-03-27 23:57:59 +0200

    wip

--- core/reconatoor-dev.py
+++ core/reconatoor-dev.py
@@ -30,7 +30,7 @@ def httpenum(targets):
     print("2DO NIKTOSCAN" )
 #    multProc("")
     #sNIKTO =
-    os.system("nikto -host %s" % targets)
+    os.system("nikto -host localhost")
     #subprocess.call(["touch" "/tmp/recodev"])
     # TODO  check proof
 
