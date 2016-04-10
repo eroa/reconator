c8db715ba738628f194834a6f0730419802cfc19
toxic <toxic@murene>
2016-04-10 04:09:06 +0200

    wip

--- core/tests/grep-dev-simple.py
+++ core/tests/grep-dev-simple.py
@@ -7,16 +7,23 @@ import multiprocessing
 import subprocess
 import csv
 
-from tests.libnmap import ndict
 
 nm = nmap.PortScanner()
-nm.scan(host='127.0.0.1' ,arguments='-sS -sV -sC -A -vvvv -oN "/tmp/grepsudo"', sudo=True)
+nm.scan(hosts='127.0.0.1' ,arguments='-sS -sV -sC -A -vvvv -oN "/tmp/grepsudo"', sudo=True)
 nxml = nm.get_nmap_last_output()
 ndict = nm.analyse_nmap_xml_scan()
 
 for host in nm.all_hosts():
     print host
+    print('----------------------------------------------------')
     for proto in nm[host].all_protocols():
-        print
-        print('proto: {0}\tstate: {1}).format(port, nm[host][proto][port]))
-
+        print('proto: {0}'.format(proto))
+        lport = list(nm[host][proto].keys())
+        print('----------------------------------------------------')
+        print lport
+        lport.sort()
+        for port in lport:
+            print type(nm[host][proto][port])
+            state = str(nm[host][proto][port])
+            print('proto: {0}\tstate: {1})'.format(port, nm[host][proto][port]))
+#TODO grep deeper dict
